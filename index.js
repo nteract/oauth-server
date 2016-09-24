b92c86f83ca951c0603a53fcbb20fcfa2cd308e9
Kyle Kelley <rgbkrk@gmail.com>
2016-09-24 16:08:21 -0700

    Close all non oauth conn paths.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const githubOAuth = require('github-oauth')({
 require('http').createServer(function(req, res) {
   if (req.url.match(/github/)) return githubOAuth.login(req, res)
   if (req.url.match(/callback/)) return githubOAuth.callback(req, res)
+  res.end(); // Close all other connection paths.
 }).listen(80)
 
 githubOAuth.on('error', function(err) {
